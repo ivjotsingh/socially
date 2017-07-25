792fe3c78b3d13f209c9f5ea15c71d810b5dbb1b
ivjotSingh <ivjotofficial@gmail.com>
2017-07-25 14:27:54 +0530

    created user model

--- social/models.py
+++ social/models.py
@@ -3,4 +3,9 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=255)
+    phone = models.CharField(max_length=30)
+    age = models.IntegerField(default=0)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on= models.DateTimeField(auto_now=True)
